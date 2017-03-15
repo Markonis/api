a0b753c9cbb9b131d31b6186a1587d548c29af9b
Marko <contact.pavlovic@gmail.com>
2017-03-15 22:33:12 +0000

    Export pipeline in index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var Controller = require('./lib/controller.js');
 var Endpoint = require('./lib/endpoint.js');
 var Namespace = require('./lib/namespace.js');
 var validate = require('./lib/validate.js');
+var pipeline = require('./lib/pipeline.js');
 
 module.exports = {
   Controller: Controller,
   Endpoint: Endpoint,
   Namespace: Namespace,
-  validate: validate
+  validate: validate,
+  pipeline: pipeline
 };
